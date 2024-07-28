f55dff523b21b2a02989c7985970c970cca24113
dueduardoful <dueduardoful@gmail.com>
2024-07-27 21:52:38 -0300

    commit alteracoes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import {Amplify} from 'aws-amplify';
-import awsConfig from './aws-exports';
-import {Authenticator} from '@aws-amplify/ui-react';
-import { signUp, SignUpInput, signOut } from 'aws-amplify/auth';
-import '@aws-amplify/ui-react/styles.css';
-Amplify.configure(awsConfig);
+// import {Amplify} from 'aws-amplify';
+// import awsConfig from './aws-exports';
+// import {Authenticator} from '@aws-amplify/ui-react';
+// import { signUp, SignUpInput, signOut } from 'aws-amplify/auth';
+// import '@aws-amplify/ui-react/styles.css';
+// Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <Authenticator>
- {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+//     <Authenticator>
+//  {({ signOut, user }) => (
+//         <main>
+//           <h1>Hello {user.username}</h1>
+//           <button onClick={signOut}>Sign out</button>
+//         </main>
+//       )}
+//     </Authenticator>
+  <div>
+    <h1>Hello</h1>
+  </div>
   );
 }
 export default App;
